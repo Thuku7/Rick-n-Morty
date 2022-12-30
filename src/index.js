dbb5a29ad8c88980aaaeb3ccd24f64a7a1774c42
Robert <robethuku11@gmail.com>
2022-12-30 21:57:55 +0300

    Create the fetch request

--- src/index.js
+++ src/index.js
@@ -7,8 +7,16 @@ let searchInput = document.getElementById('fname');
 characterForm.addEventListener('submit', function(event) {
     event.preventDefault()
     let submitedCharacter = searchInput.value
+    showCharacter(submitedCharacter)
     
 })
+
+
+function showCharacter(submitedCharacter) {
+  fetch(`https://rickandmortyapi.com/api/character/?name=${submitedCharacter}`)
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
 
 
 
