4675101f11377b48a5145896dba6253ab984e7ad
Robert <robethuku11@gmail.com>
2022-12-31 12:00:25 +0300

    Add the display features to the js file

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ characterForm.addEventListener('submit', function(event) {
     event.preventDefault()
     let submitedCharacter = searchInput.value
     showCharacter(submitedCharacter)
+
+    this.reset();
     
 })
 
@@ -19,7 +21,23 @@ function showCharacter(submitedCharacter) {
 }
 
 function renderCharacter(data) {
-  let displayCharacter = document.createElement('li')
+  let displayCharacter = document.createElement('li');
+  displayCharacter.innerHTML = `
+  <div class="DisplayCharacter">
+  <div class="Character-image"><img src="${data.results[0].image}" height="400px" width="400px"></div>
+  <div class="character-info">
+  <p> Character Name: ${data.results[0].name}</p>
+  <p> Status: ${data.results[0].status}</p>
+  <p> Species: ${data.results[0].species}</p>
+  <p> Origin: ${data.results[0].origin.name}</p>
+  </div>
+ 
+
+  </div>`
+  
+document.getElementById('search-form').appendChild(displayCharacter)
+ 
+   
 }
 
 
